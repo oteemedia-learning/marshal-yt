94b1f985b8264e58cda7a1c9c571beb12265cba8
churchillotiende <churchillotiende84@gmail.com>
2026-05-27 11:24:58 +0300

    chore: use the correct components

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,12 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function LoginPage() {
-  return <div>Hello from the login page</div>
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!!!</CardTitle>
+        <CardDescription>Login with your github email account</CardDescription>
+      </CardHeader>
+    </Card>
+  )
 }
